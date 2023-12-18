4cdc25ecf61a94ff7e8b2c663068f3bea994d9cc
JoyRume <joy.rume@student.moringaschool.com>
2023-12-18 13:46:34 +0300

    Code Challege Complete

---
+++ StudentGradeGenerator .js
@@ -0,0 +1,20 @@
+function CalculateGrades(mark) {
+    
+  
+    // Determine and return grade
+  
+    if (mark > 79){
+       return 'A';
+    }
+    else if (mark >= 60 && mark <=79){
+      return 'B';
+    }
+    else if (mark >=59 &&  mark <=49){
+      return 'C';
+    else if (mark >=40 && mark <=49) {
+      return 'D';
+    }
+    else{
+        return E
+    }
+}   
